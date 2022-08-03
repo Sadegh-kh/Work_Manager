4ba13ac9aebc54e81fe69e6a67a600b5d903cb22
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-03 06:48:53 +0430

    test Work Manager

--- app/src/main/java/com/example/work_manager/MainActivity.kt
+++ app/src/main/java/com/example/work_manager/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.work_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import androidx.work.Worker
+import com.example.work_manager.worker.NotificationWorker
 
 class MainActivity : AppCompatActivity() {
+    lateinit var workManager:WorkManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        workManager=WorkManager.getInstance(this)
 
+        //PeriodicWorkRequest or OneTimeWorkRequest
+        val notificationWorker=OneTimeWorkRequest.from(NotificationWorker::class.java)
+        workManager.enqueue(notificationWorker)
     }
 }
---
+++ app/src/main/java/com/example/work_manager/worker/NotificationWorker.kt
@@ -0,0 +1,24 @@
+package com.example.work_manager.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+    override fun doWork(): Result {
+        return try {
+            doMyJop()
+            Result.success()
+        }catch (ex:Exception){
+            Result.failure()
+        }
+    }
+
+    private fun doMyJop() {
+        for(i in 0..10000000){
+            Log.v("testWorker","Number is $i")
+        }
+    }
+}
